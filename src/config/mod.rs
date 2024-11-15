982aa3169127f5c4657dc8e1ea149bf7596e1efc
Dylan <dylant7192@gmail.com>
2024-11-15 13:49:46 +0000

    Added some elaboration for env args tbi

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,5 @@
-// code to collect env args here
+// Code to collect env args here
+// Essentially, add parse for inline code passed via string 
+// or if element 0 is -f, treat element 1 as a file path and 
+// read code from there.
+// Also , add args for compile-time vs runtime input
